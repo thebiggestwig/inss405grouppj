7daf04dd68dc85a32415cf45012e7ad38c15e1de
thebiggestwig <irlyrlyhatefacebook@gmail.com>
2023-12-11 21:08:10 -0500

    blackjack code - update 1

--- blackjack_game.py
+++ blackjack_game.py
@@ -2,26 +2,48 @@ import random
 
 deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11] * 4
 
+# this is the code that creates an array of possible responses from the deck.
+# Since the value of cards range from 2-11, with 4 of each suit. This is the
+# artihmetic we chose to cover all deck card outcomes.
+# - Chimdi
 
 def deal_cards(deck, num_cards=2):
     hand = random.sample(deck, num_cards)
     return hand
 
+# this is the code that declares the hand of the dealer and the player
+# while using the function to randomize the array of deck responses. Each
+# person (the dealer and player) get a designated hand
+# - Chimdi
 
 def calculate_total(hand):
     return sum(hand)
 
+# Previously, as in Blackjack, we used 'num_cards' to declare only two cards
+# per participant. This function is run here to take our 'hand' varaible and
+# add up the values to generate a sum to be later set to the rules of
+# the game of Blackjack.
+# - Chimdi
 
 def hit(hand):
     card = random.choice(deck)
     hand.append(card)
     return hand
 
+# This part of the code is used to designate the effect of deciding to 'hit'
+# The results are a random card being selected from the aformentioned deck
+# And appended to the 'hand' that both participants have
+# - Chimdi
 
 def print_results(dealer_hand, player_hand):
     print("Dealer's hand:", dealer_hand)
     print("Your hand:", player_hand)
 
+# This part of the code initiates the game by handling the processing and
+# random selection of cards for each player in the background. Instead, upon
+# Start of the code running, a message is printed letting the player see what
+# Their cards are, along with the dealer's first two cards.
+# - Chimdi
 
 def check_blackjack(dealer_hand, player_hand):
     if calculate_total(player_hand) == 21:
